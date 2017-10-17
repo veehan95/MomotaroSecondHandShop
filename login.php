8f0a8fa769e5fa4c92de10e77382143ead8d2cce
veehan95 <cloud_strife@hotmail.my>
2017-10-17 20:02:41 +0800

    first commit

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="include/login-style.css">
+</head>
+<body>
+  <div id="header">
+    <p>Momotaro Second Hand Shop</p>
+  </div>
+  <div id="content">
+    <form>
+      <input name="username" type="text" placeholder="Username">
+      <br/>
+      <input name="password" type="password" placeholder="Password">
+      <br/>
+      <input type="submit">
+    </form>
+  </div>
+</body>
+</html>
